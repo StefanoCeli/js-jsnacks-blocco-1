649000e145a8e0f62ff753a4c4d4e82e2173aae0
StefanoCeli <stefanopsn91@gmail.com>
2021-04-13 11:58:52 +0200

    fine jsnack 6

--- jsnack 6/js/script.js
+++ jsnack 6/js/script.js
@@ -3,7 +3,7 @@ var num_utente= parseInt(prompt('inserisci un numero'));
 
 
 while(num_utente >= 1){
-    var potenza= Math.pow(num_utente,1);
+    var potenza= Math.pow(num_utente,3);
     console.log(potenza);
     num_utente--;
     
